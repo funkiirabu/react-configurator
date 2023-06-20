06f30d206cf837859b86bfb284246d3bfdd072d6
funkiirabu <phil.vadala@optus.com.au>
2023-06-21 08:03:08 +1000

    Importing basic threejs scene and adding Shirt function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Canvas from './Canvas';
+import { App as Canvas } from './Canvas';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
