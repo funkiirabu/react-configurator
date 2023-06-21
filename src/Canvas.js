dcf2da2d898d40bd10c3ef1b432017a9b5900a73
funkiirabu <phil.vadala@optus.com.au>
2023-06-21 15:25:15 +1000

    Adding Backdrop component and lighting

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,9 +1,10 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Center, useGLTF, Environment } from '@react-three/drei'
+import { OrbitControls, Center, useGLTF, Environment, AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 import { useRef } from 'react'
 
 export const App = ({ position = [-1,0,2.5], fov = 25 }) => (
-  <Canvas 
+  <Canvas
+    shadows
     eventSource={document.getElementById('root')}
     eventPrefix='client'
     camera={{ position, fov }}>
@@ -11,6 +12,7 @@ export const App = ({ position = [-1,0,2.5], fov = 25 }) => (
     <Environment preset="city" />
     <Center>
       <Shirt />
+      <Backdrop />
     </Center>
     <OrbitControls />
   </Canvas>
@@ -27,6 +29,33 @@ function Shirt(props) {
         material={materials.lambert1}
       />
     </group>
+  )
+}
+
+function Backdrop() {
+  return (
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2,0,0]}
+      position={[0,0,-0.4]}>
+      <RandomizedLight 
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5,5,-10]}
+      />
+      <RandomizedLight 
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[5,5,-9]}
+      />
+    </AccumulativeShadows>
   )
 }
 
