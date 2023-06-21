f72f45dbdc08dbb5d7c6a845e046b55624a88264
funkiirabu <phil.vadala@optus.com.au>
2023-06-21 15:06:53 +1000

    Adding gltf declarative component

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,11 +1,14 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Center } from '@react-three/drei'
+import { OrbitControls, Center, useGLTF, Environment } from '@react-three/drei'
+import { useRef } from 'react'
 
 export const App = ({ position = [-1,0,2.5], fov = 25 }) => (
   <Canvas 
     eventSource={document.getElementById('root')}
     eventPrefix='client'
     camera={{ position, fov }}>
+    <ambientLight intensity={0.5} />
+    <Environment preset="city" />
     <Center>
       <Shirt />
     </Center>
@@ -13,11 +16,18 @@ export const App = ({ position = [-1,0,2.5], fov = 25 }) => (
   </Canvas>
 )
 
-function Shirt(){
-  return(
-    <mesh>
-      <boxGeometry args={[1,1,1]} />
-      <meshBasicMaterial />
-    </mesh>
+function Shirt(props) {
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_shirt_male.geometry}
+        material={materials.lambert1}
+      />
+    </group>
   )
 }
+
+useGLTF.preload('/shirt_baked.glb');
